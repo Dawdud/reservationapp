bbee8b5290365610317dc65657148eabf4059bde
Germanicus <dawdud12@gmail.com>
2020-08-16 20:48:58 +0200

    no message

--- server/app.js
+++ server/app.js
@@ -3,16 +3,23 @@ import cors from "cors";
 
 import bodyParser from "body-parser";
 import logger from "morgan";
-import passport from "./config/passport";
-import reservation from "./routes/findReservation";
-import createReservation from "./routes/createReservation";
-import db from "./models";
-import users from "./routes/findUser";
-import register from "./routes/createUser";
-import auth from "./routes/auth";
 const PORT = process.env.PORT || 8080;
+const cookieParser = require("cookie-parser");
+
+const reservation = require("./routes/findReservation");
+const createReservation = require("./routes/createReservation");
+const register = require("./routes/createUser");
+const users = require("./routes/findUser");
+const currentUser = require("./routes/currentUser");
+const db = require("./models");
+const uuid = require("uuid/v4");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const passport = require("./config/passport");
+const auth = require("./routes/auth");
 const app = express();
 
+app.use(cookieParser());
 app.use(cors());
 
 // view engine setup
@@ -24,7 +31,7 @@ app.use(bodyParser.json());
 app.use(logger("dev"));
 
 app.use(passport.initialize());
-//app.use(passport.session());
+///app.use(passport.session());
 
 app.use(
   "/reservation",
@@ -37,7 +44,11 @@ app.use(
   createReservation
 );
 app.use("/users", passport.authenticate("jwt", { session: false }), users);
-
+app.use(
+  "/isauth",
+  passport.authenticate("jwt", { session: false }),
+  currentUser
+);
 app.use("/login", auth);
 app.use("/register", register);
 
